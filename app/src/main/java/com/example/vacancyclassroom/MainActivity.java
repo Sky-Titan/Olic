58f0437d5ccf8cf0328057c57da4482c8ebf1407
Sky-Titan <er65119@naver.com>
2019-03-21 16:07:48 +0900

    parsing test

---
+++ app/src/main/java/com/example/vacancyclassroom/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.vacancyclassroom;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String htmlPageUrl = "http://my.knu.ac.kr/stpo/stpo/cour/listLectPln/list.action?search_open_crse_cde=1O07&sub=1O&search_open_yr_trm=20191"; //파싱할 홈페이지의 URL주소
+    private TextView textviewHtmlDocument;
+    private String htmlContentInStringFormat="";
+
+    int cnt=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        textviewHtmlDocument = (TextView)findViewById(R.id.textView);
+        textviewHtmlDocument.setMovementMethod(new ScrollingMovementMethod()); //스크롤 가능한 텍스트뷰로 만들기
+
+        Button htmlTitleButton = (Button)findViewById(R.id.button);
+        htmlTitleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println( (cnt+1) +"번째 파싱");
+                JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+                jsoupAsyncTask.execute();
+                cnt++;
+            }
+        });
+    }
+
+    private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+
+                Document doc = Jsoup.connect(htmlPageUrl).get();
+
+
+                //테스트1
+                Elements titles= doc.select("td.th17");
+
+                System.out.println("-------------------------------------------------------------");
+                for(Element e: titles){
+                    System.out.println("title: " + e.text());
+                    htmlContentInStringFormat += e.text().trim() + "\n";
+                }
+
+
+                System.out.println("-------------------------------------------------------------");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            textviewHtmlDocument.setText(htmlContentInStringFormat);
+        }
+    }
+}
